011920d88525d0e5317bd80d09fa4e3d62a12f83
wayne_bui_vn <dao.bui@savvycomsoftware.com>
2023-04-13 14:38:23 +0700

    remove redundant code

--- com.leetcode.algorithm1/SecondHighest.java
+++ com.leetcode.algorithm1/SecondHighest.java
@@ -1,8 +1,6 @@
 package com.leetcode.algorithm1;
 
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 
 public class SecondHighest {
     public static void main(String[] args) {
